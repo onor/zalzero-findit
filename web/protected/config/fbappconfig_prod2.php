6bcef9e444f8c11aef7306857d33913e7bec2186
snehesh <snehesh@mobicules.com>
2012-10-07 16:05:21 +0530

    Fixed FB Config for Prod2

--- web/protected/config/fbappconfig_prod2.php
+++ web/protected/config/fbappconfig_prod2.php
@@ -5,7 +5,7 @@ class getFbCredentials {
       
  public function  getFbAppData() {
 
-        $this->fbAppIDcredetials = array('fbAppID'=>'416871301709195','fbAppSecretId'=>'a8b54e647bc29ee2224b28a522b8d502','fbCanvasPage'=>'http://apps.facebook.com/zalerioprod2/','fbCanvasUrl'=>'http://zzprod2.zalerio.com/','fbAppName'=>'ZalerioProd2');
+        $this->fbAppIDcredetials = array('fbAppID'=>'416871301709195','fbAppSecretId'=>'a8b54e647bc29ee2224b28a522b8d502','fbCanvasPage'=>'http://apps.facebook.com/zalerioprod/','fbCanvasUrl'=>'http://zzprod2.zalerio.com/','fbAppName'=>'ZalerioProd2');
         return $this->fbAppIDcredetials;
  }
 }
