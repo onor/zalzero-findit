7e9cdc030719902c76a81f3bd620a6d88261055c
= <mymail.anupam@gmail.com>
2012-10-08 12:26:43 +0530

    change facebook config class

--- web/protected/config/fbappconfig_staging.php
+++ web/protected/config/fbappconfig_staging.php
@@ -1,11 +1,20 @@
 <?php
-class getFbCredentials {
 
- private $fbAppIDcredetials;
+class facebookCredetials {
 
- public function  getFbAppData() {
+	public $config;
 
-        $this->fbAppIDcredetials = array('fbAppID'=>'392619034110411','fbAppSecretId'=>'f6cdf2e164dccb995a69f56128e8bafe','fbCanvasPage'=>'http://apps.facebook.com/zalzerostaging/','fbCanvasUrl'=>'http://staging.zalerio.com/','fbAppName'=>'Find It');
-        return $this->fbAppIDcredetials;
- }
+	public function  __construct() {
+
+		$config = new stdClass();
+
+		$config->appId			=	'392619034110411';
+		$config->appSecretId	=	'f6cdf2e164dccb995a69f56128e8bafe';
+		$config->canvasPage		=	'http://apps.facebook.com/zalzerostaging/';
+		$config->canvasUrl		=	'http://staging.zalerio.com/';
+		$config->appName		=	'Find It';
+		
+		$this->config = $config;
+
+	}
 }
