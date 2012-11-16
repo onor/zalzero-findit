a5dfcc1d5077e833553da67390a9d8e936fdefb8
Abhilash Bhaduri <abhilash@mobicules.com>
2012-11-16 15:43:44 +0530

    added changes to nonSecuredGameEnterTest

--- test/src/test/java/com/zalerio/test/GameEnterTest.java
+++ test/src/test/java/com/zalerio/test/GameEnterTest.java
@@ -19,24 +19,13 @@ public class GameEnterTest extends ZalerioBaseTest {
 	@Test
 	public  void nonSecuredGameOpenTest() {
 		
-		driver2.quit();
-		// Click Start a New Game
-	/*	WebElement startButton = driver1.findElement(By.id("startButton"));
-		if(startButton.isDisplayed())
-		{
-			String jobID = ( driver1).getSessionId().toString();
-	        SauceREST client = new SauceREST("cloudbees_zalzero", "15b88203-7a0f-4339-804a-fffb6abb2e3c");
-	        Map<String, Object>sauceJob = new HashMap<String, Object>();
-	        sauceJob.put("name", "Test method: "+testName.getMethodName());
-	        try {
-				client.jobPassed(jobID);
-				 client.updateJobInfo(jobID, sauceJob);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-			e.printStackTrace();
-			}
+		
+		// Check if start-a-game button is visible
+		WebElement startButton = driver1.findElement(By.id("startButton"));
+		assertEquals(startButton.isDisplayed(),true);
+		
 	       
-		}*/
+		
 	}
 	
 	@Test
