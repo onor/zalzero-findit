b8d77aeeb19d741be74a93f73ac701ae6fd1656b
pankaj-mobicules <pankajanupam@mobicules.com>
2012-11-21 11:52:14 +0530

    change access rules

--- web/protected/controllers/SiteController.php
+++ web/protected/controllers/SiteController.php
@@ -25,6 +25,7 @@ class SiteController extends Controller
 	public function filters()
 	{
 		return array(
+			
 				'accessControl', // perform access control for CRUD operations
 		);
 	}
@@ -33,16 +34,10 @@ class SiteController extends Controller
 	public function accessRules()
 	{
 		return array(
-				array('allow',  // allow all users to perform 'index' and 'view' actions
-						'actions'=>array('index','error','login'),
-						'users'=>array('*'),
-				),
+			
 				array('allow',  // allow all users to perform 'index' and 'view' actions
 						'actions'=>array('privacypolicy','tos'),
 						'users'=>array('@'),
-				),
-				array('deny',  // deny all users
-						'users'=>array('*'),
 				),
 		);
 	}
