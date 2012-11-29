6c7adca04f715c03057ab12b03e3b7aea28abb02
pankaj-mobicules <pankajanupam@mobicules.com>
2012-11-29 15:38:55 +0530

    change auth to test request

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -9,6 +9,7 @@ class DeauthoriseController extends Controller
 		//echo Yii::app()->user->getId();
 		Yii::log(Yii::app()->user->getId().'gdfgdf', CLogger::LEVEL_ERROR, 'Any category/label will work');
 		
+		Yii::log($_REQUEST, CLogger::LEVEL_ERROR, 'Any category/label will work');
 	/* 	
 		list($encoded_sig, $payload) = explode('.', $_REQUEST["signed_request"], 2);
 		$signedRequestData = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
@@ -20,7 +21,7 @@ class DeauthoriseController extends Controller
 	}
 
 	
- /*	public function filters()
+ public function filters()
 	{
 		// return the filter configuration for this controller, e.g.:
 		return array( 'accessControl');
@@ -33,5 +34,5 @@ class DeauthoriseController extends Controller
         					'users'=>array('*'),
         			),
         	);
-    } */
+    }
 }
