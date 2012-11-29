85bbe243def48349cdc6c054d59937b62043dc70
pankaj-mobicules <pankajanupam@mobicules.com>
2012-11-29 17:36:01 +0530

    update database on leave

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -10,18 +10,10 @@ class DeauthoriseController extends Controller
 		list($encoded_sig, $payload) = explode('.', $_REQUEST["signed_request"], 2);
 		$signedRequestData = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
 		
-		ob_start();
-var_dump($signedRequestData["user_id"]);
-$result = ob_get_clean();
-		
-		Yii::log($result, CLogger::LEVEL_ERROR, 'var_export');
-		
-	/*	foreach($signedRequestData as $key=>$val){
-			Yii::log($key.'=>'.$val, CLogger::LEVEL_ERROR, 'Any category/label will work');
-		}
-		
-		Yii::log($signedRequestData["user_id"], CLogger::LEVEL_ERROR, 'user leving the game');
+			
 
+		Yii::log($signedRequestData["user_id"], CLogger::LEVEL_ERROR, 'user leving the game');
+		
 		$check_for_user_existence = Zzuser::model()->findByAttributes( array('user_fbid'=>$signedRequestData["user_id"]) );
 		
 		if(isset($check_for_user_existence)) {
@@ -29,7 +21,11 @@ $result = ob_get_clean();
 			$check_for_user_existence->save();
 			
 			Yii::log($signedRequestData["user_id"], CLogger::LEVEL_ERROR, 'User leave the game: Status changed to invited ');
-		}*/
+		}
+		
+		foreach($signedRequestData as $key=>$val){
+			Yii::log($key.'=>'.$val, CLogger::LEVEL_ERROR, 'Any category/label will work');
+		}
 	}
 
 	
