272206f52dae8ed004b21fa59a812904c3aedf31
pankaj-mobicules <pankajanupam@mobicules.com>
2012-11-29 15:57:13 +0530

    change log in deauth

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -6,15 +6,19 @@ class DeauthoriseController extends Controller
 	{
 		//$this->render('index');
 		
+		
+		list($encoded_sig, $payload) = explode('.', $_REQUEST["signed_request"], 2);
+		$signedRequestData = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
+		
 		//echo Yii::app()->user->getId();
 		Yii::log(Yii::app()->user->getId().'gdfgdf', CLogger::LEVEL_ERROR, 'Any category/label will work');
 
 
-foreach($_REQUEST as $val){
+foreach($signedRequestData as $val){
 	Yii::log($val, CLogger::LEVEL_ERROR, 'Any category/label will work');
 }
 		
-
+Yii::log($_REQUEST["signed_request"], CLogger::LEVEL_ERROR, 'Any category/label will work');
 	/* 	
 		list($encoded_sig, $payload) = explode('.', $_REQUEST["signed_request"], 2);
 		$signedRequestData = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
