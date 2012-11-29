eee6c69ae7a743d0433a16efcd5f3ec9124026a0
pankaj-mobicules <pankajanupam@mobicules.com>
2012-11-29 14:55:24 +0530

    add sign request in deauth

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -5,13 +5,20 @@ class DeauthoriseController extends Controller
 	public function actionIndex()
 	{
 		//$this->render('index');
+		
 		//echo Yii::app()->user->getId();
+		
+		list($encoded_sig, $payload) = explode('.', $_REQUEST["signed_request"], 2);
+		$signedRequestData = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
+		
+		Yii::log($signedRequestData, CLogger::LEVEL_ERROR, 'Any category/label will work');
+		
 		Yii::log(Yii::app()->user->getId(), CLogger::LEVEL_ERROR, 'Any category/label will work');
 	
 	}
 
 	
- 	public function filters()
+ /*	public function filters()
 	{
 		// return the filter configuration for this controller, e.g.:
 		return array( 'accessControl');
@@ -24,5 +31,5 @@ class DeauthoriseController extends Controller
         					'users'=>array('*'),
         			),
         	);
-    } 
+    } */
 }
