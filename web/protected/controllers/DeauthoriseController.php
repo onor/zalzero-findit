f23e0d48c3aaa4a65303470e7f193e7ee3841042
pankaj-mobicules <pankajanupam@mobicules.com>
2012-11-29 14:15:46 +0530

    add Deauthorise Controller

---
+++ web/protected/controllers/DeauthoriseController.php
@@ -0,0 +1,27 @@
+<?php
+
+class DeauthoriseController extends Controller
+{
+	public function actionIndex()
+	{
+		//$this->render('index');
+		Yii::app()->user->getId();
+	
+	}
+
+	
+/* 	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array( 'accessControl');
+	}
+	
+ 	public function accessRules()
+    {
+        	return array(
+        			array('allow',
+        					'users'=>array('*'),
+        			),
+        	);
+    } */
+}
