6719d389f73989de765eb4fe7ad4ea59c3d33994
pankaj-mobicules <pankajanupam@mobicules.com>
2012-11-29 15:48:22 +0530

    dum array deauth

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -8,8 +8,13 @@ class DeauthoriseController extends Controller
 		
 		//echo Yii::app()->user->getId();
 		Yii::log(Yii::app()->user->getId().'gdfgdf', CLogger::LEVEL_ERROR, 'Any category/label will work');
+
+
+foreach($_REQUEST as $val){
+	Yii::log($val, CLogger::LEVEL_ERROR, 'Any category/label will work');
+}
 		
-		Yii::log($_REQUEST, CLogger::LEVEL_ERROR, 'Any category/label will work');
+
 	/* 	
 		list($encoded_sig, $payload) = explode('.', $_REQUEST["signed_request"], 2);
 		$signedRequestData = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
