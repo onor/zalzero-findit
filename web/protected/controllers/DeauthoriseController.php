3ca33071c411eb1488d4964a50569c8848138f53
pankaj-mobicules <pankajanupam@mobicules.com>
2012-11-29 15:26:51 +0530

    chagne in deauth

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -7,15 +7,15 @@ class DeauthoriseController extends Controller
 		//$this->render('index');
 		
 		//echo Yii::app()->user->getId();
-		Yii::log(Yii::app()->user->getId(), CLogger::LEVEL_ERROR, 'Any category/label will work');
+		Yii::log(Yii::app()->user->getId().'gdfgdf', CLogger::LEVEL_ERROR, 'Any category/label will work');
 		
-		
+	/* 	
 		list($encoded_sig, $payload) = explode('.', $_REQUEST["signed_request"], 2);
 		$signedRequestData = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
 		
 		Yii::log($signedRequestData, CLogger::LEVEL_ERROR, 'Any category/label will work');
 		
-		Yii::log(Yii::app()->user->getId(), CLogger::LEVEL_ERROR, 'Any category/label will work');
+		Yii::log(Yii::app()->user->getId(), CLogger::LEVEL_ERROR, 'Any category/label will work'); */
 	
 	}
 
