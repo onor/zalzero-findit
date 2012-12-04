c7673cef1dfbfc5f98bb767deef45fcd46b3902d
Developer <Mobicules@dhcppc0.(none)>
2012-12-04 19:04:21 +0530

    add unsubscribe

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -4,13 +4,10 @@ class DeauthoriseController extends Controller
 {
 	public function actionIndex()
 	{
-		//$this->render('index');
-		
 		
 		list($encoded_sig, $payload) = explode('.', $_REQUEST["signed_request"], 2);
 		$signedRequestData = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
 		
-			
 
 		Yii::log($signedRequestData["user_id"], CLogger::LEVEL_ERROR, 'user leving the game');
 		
@@ -28,8 +25,23 @@ class DeauthoriseController extends Controller
 		}
 	}
 
+	public function actionUnsubscribe()
+	{		
+		if( isset($_REQUEST['user_id']) ) {
+			
+			$usr = Zzuser::model()->findByAttributes( array('user_fbid'=>$_REQUEST['user_id']) );
+			
+			if(isset($usr)) {
+			
+				$check_for_user_existence->zzuser_subscribe_status = '0';
+				$check_for_user_existence->save();
+			
+				Yii::log($_REQUEST['user_id'], CLogger::LEVEL_ERROR, 'User '.$_REQUEST['user_id'].' unsubscribe ');
+			}
+		}
+	}
 	
- public function filters()
+ 	public function filters()
 	{
 		return array( 'accessControl');
 	}
@@ -38,8 +50,11 @@ class DeauthoriseController extends Controller
     {
         	return array(
         			array('allow',
-        					'users'=>array('*'),
+        				  'actions'=>array('index','unsubscribe'),
+        				  'users'=>array('*'),
         			),
         	);
     }
+    
+    
 }
