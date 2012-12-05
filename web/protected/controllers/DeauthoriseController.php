acca45d6f1f6dff3019fb90c3fa1f2ad95484ebd
pankaj-mobicules <pankajanupam@mobicules.com>
2012-12-05 13:33:36 +0530

    change variable name in deauth

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -33,15 +33,14 @@ class DeauthoriseController extends Controller
 			
 			if(isset($usr)) {
 
-				$check_for_user_existence->zzuser_subscribe_status = 'FALSE';
-				$check_for_user_existence->save();
+				$usr->zzuser_subscribe_status = 'FALSE';
+				$usr->save();
 			
 				Yii::log($_REQUEST['user_id'], CLogger::LEVEL_ERROR, 'User '.$_REQUEST['user_id'].' unsubscribe ');
 			
 				echo '<h1>You have been successfully unsubscribed!</h1>';
 
-				echo '<p>You have requested to be removed from our mailing list, which has been successful, \
-				      <br />You will not receive any future correspondence from us.</p>';
+				echo '<p>You have requested to be removed from our mailing list, which has been successful, <br />You will not receive any future correspondence from us.</p>';
 				
 			}
 		}
