b451195eab1e5216bd823bfd341e36759741677f
Developer <Mobicules@dhcppc0.(none)>
2012-12-05 13:08:02 +0530

    add unsubscribe logic

--- web/protected/controllers/DeauthoriseController.php
+++ web/protected/controllers/DeauthoriseController.php
@@ -26,17 +26,23 @@ class DeauthoriseController extends Controller
 	}
 
 	public function actionUnsubscribe()
-	{		
+	{	
 		if( isset($_REQUEST['user_id']) ) {
 			
 			$usr = Zzuser::model()->findByAttributes( array('user_fbid'=>$_REQUEST['user_id']) );
 			
 			if(isset($usr)) {
-			
-				$check_for_user_existence->zzuser_subscribe_status = '0';
+
+				$check_for_user_existence->zzuser_subscribe_status = 'FALSE';
 				$check_for_user_existence->save();
 			
 				Yii::log($_REQUEST['user_id'], CLogger::LEVEL_ERROR, 'User '.$_REQUEST['user_id'].' unsubscribe ');
+			
+				echo '<h1>You have been successfully unsubscribed!</h1>';
+
+				echo '<p>You have requested to be removed from our mailing list, which has been successful, \
+				      <br />You will not receive any future correspondence from us.</p>';
+				
 			}
 		}
 	}
