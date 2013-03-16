8f0d31a4bdba7d422316f635091e11d04d6ada24
piyush123 <Piyush@zalzero.com>
2013-03-16 20:02:08 +0530

    Update zaleroJs_header.js
    adding clicky

--- web/js/zaleroJs_header.js
+++ web/js/zaleroJs_header.js
@@ -1,3 +1,8 @@
+<a title="Web Analytics" href="http://clicky.com/100591348"><img alt="Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(100591348); }catch(e){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100591348ns.gif" /></p></noscript>
+
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
