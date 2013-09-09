d976abb02744e706cafc9387e346c5d232dea450
piyush123 <piyush@zalzero.com>
2013-09-09 21:46:27 +0530

    removing old union

--- union_2.0.0.tls/stopserver.bat
+++
@@ -1,5 +0,0 @@
-SET HOST=localhost
-SET PORT=8000
-SET PASSWORD=Cytzmlk1
-java -cp lib\union.jar;lib\mariner.jar net.user1.union.core.UnionMain %HOST% %PORT% %PASSWORD% a5
-
--- union_2.0.0.tls/stopserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-HOST=localhost
-PORT=8000
-PASSWORD=Cytzmlk1
-java -cp lib/union.jar:lib/mariner.jar net.user1.union.core.UnionMain $HOST $PORT $PASSWORD a5
